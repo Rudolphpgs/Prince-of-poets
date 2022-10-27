996511a66613cd15babc38ab9e07e367aaf68898
unknown <rudolph@pgsuae.com>
2022-10-27 18:03:35 +0400

    first commit

---
+++ src/assets/scripts/home.js
@@ -0,0 +1,18 @@
+(function ($) {
+  $(function () {
+    setTimeout(function () {
+      $("body").addClass("loadScale");
+      setTimeout(function () {
+        $("body").addClass("is-loaded");
+        setTimeout(function () {
+          $(".loader").remove();
+        }, 400);
+      }, 600);
+    }, 250);
+
+    // $(".homeBanner").viewportChecker({
+    //   classToAdd: "inView",
+    //   offset: "30%",
+    // });
+  });
+})(jQuery);
